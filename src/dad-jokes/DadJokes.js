82a93b6805dcbb849e5cb5093726365b5e280e77
ahmady016 <ahmady09@gmail.com>
2019-08-10 06:33:19 +0200

    add jokesStats, modify some styles

--- src/dad-jokes/DadJokes.js
+++ src/dad-jokes/DadJokes.js
@@ -8,6 +8,15 @@ import Spinner from '../_shared/Spinner'
 //#region jokes logic
 const JOKE_API_URL = 'https://icanhazdadjoke.com'
 const JOKES_LIMIT = 10
+const INIT_JOKES_STATS = {
+  'em-rolling_on_the_floor_laughing': 0,
+  'em-laughing': 0,
+  'em-smiley': 0,
+  'em-slightly_smiling_face': 0,
+  'em-neutral_face': 0,
+  'em-confused': 0,
+  'em-angry': 0
+}
 
 const getJoke = async () => {
   try {
@@ -43,6 +52,7 @@ const fetchJokes = async (jokes, setState) => {
   const newJokes = await getJokes( new Set(jokes.map(joke => joke.joke)) )
   setState(
     state => ({
+      ...state,
       loading: false,
       jokes: [...state.jokes, ...newJokes]
     })
@@ -65,24 +75,82 @@ const getVoteStyles = (vote) => {
   else
     return { emoji: 'em em-angry', color: '#8f0900' }
 }
+
+const setJokesStats = (jokes, setState) => {
+  const jokesStats = jokes.reduce( (stats, { vote }) =>  {
+    if (vote >= 15)
+      stats['em-rolling_on_the_floor_laughing'] += 1
+    else if (vote >= 12)
+      stats['em-laughing'] += 1
+    else if (vote >= 9)
+      stats['em-smiley'] += 1
+    else if (vote >= 6)
+      stats['em-slightly_smiling_face'] += 1
+    else if (vote >= 3)
+      stats['em-neutral_face'] += 1
+    else if (vote >= 0)
+      stats['em-confused'] += 1
+    else
+      stats['em-angry'] += 1
+    return stats
+  }, { ...INIT_JOKES_STATS })
+  setState(
+    state => ({
+      ...state,
+      jokesStats
+    })
+  )
+}
 //#endregion
 
 //#region styled components
-const JokesCard = styled.div`
-  width: 70%;
-  margin: auto !important;
+const JokesWrapper = styled.div`
+  height: 85vh;
+  width: 90vw;
+  margin: auto;
+`
+const JokesStats = styled.div`
+  background-color: #493267 !important;
+  color: #ffa700 !important;
+  height: 100%;
 `
 const CardHeader = styled.div`
-  background-color: #ffa700 !important;
   padding: 0 !important;
+`
+const JokesIconCircle = styled.div`
+  width: 120px;
+  height: 120px;
+  border: 12px solid #5d437f;
+  box-shadow: inset 0px -20px 0px 20px #5d437f;
+  background-color: #473064;
+  border-radius: 100%;
+  margin-bottom: 0.7rem !important;
+`
+const NewJokesBtn = styled.button`
+  background-color: #ffcc5c !important;
+  color: #493267 !important;
+  &:hover {
+    background-color: #ffa700 !important;
+    color: #ffffff !important;
+  }
+`
+const StatsBody = styled.ul`
+  list-style-type: none;
+  height: 90%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: auto !important;
+  margin-top: auto !important;
+  padding-inline-start: 0 !important;
+`
+const JokesCard = styled.div`
+  height: 94%;
 `
 const CardBody = styled.div`
   background-color: #008744 !important;
-  height: 67vh;
   overflow-y: auto;
-`
-const CardFooter = styled.div`
-  background-color: #0057e7 !important;
 `
 const VoteIcon = styled.i`
   cursor: pointer;
@@ -121,7 +189,8 @@ function Joke({ doVote, id, joke, vote }) {
 export default function DadJokes() {
   const [state, setState] = React.useState({
     loading: false,
-    jokes: JSON.parse(localStorage.getItem('jokes') || '[]')
+    jokes: JSON.parse(localStorage.getItem('jokes') || '[]'),
+    jokesStats: { ...INIT_JOKES_STATS }
   })
 
   React.useEffect( () => {
@@ -130,7 +199,10 @@ export default function DadJokes() {
   }, [state.jokes, state.jokes.length])
 
   React.useEffect(
-    () => void localStorage.setItem('jokes', JSON.stringify(state.jokes)),
+    () => {
+      localStorage.setItem('jokes', JSON.stringify(state.jokes))
+      setJokesStats(state.jokes, setState)
+    },
     [state.jokes]
   )
 
@@ -150,27 +222,43 @@ export default function DadJokes() {
   }, [])
 
   return (
-    <JokesCard className='card'>
-      <CardHeader className='card-header flex-around'>
-        <i className="em em-joy font-s-25"></i>
-        <span className='card-title display-4 text-dark font-w-6'>Dad Jokes</span>
-        <button
-          className='btn btn-success btn-lg font-w-6'
-          onClick={() => fetchJokes(state.jokes, setState)}
-        >
-          New Jokes
-        </button>
-      </CardHeader>
-      <CardBody className='card-body text-light scroll p-0'>
-        { (state.loading)
-          ? <Spinner />
-          : state.jokes.map(joke => <Joke key={joke.id} doVote={doVote} {...joke} />)
-        }
-      </CardBody>
-      <CardFooter className='card-footer text-light'>
-        <span>Count: {state.jokes.length}</span>
-      </CardFooter>
-    </JokesCard>
+    <JokesWrapper className='flex-center'>
+      <JokesStats className='card flex-b-35'>
+        <CardHeader className='card-header flex-column-center'>
+          <span className='font-w-6 font-s-25'>Dad Jokes</span>
+          <JokesIconCircle>
+            <i className="em em-joy font-s-4"></i>
+          </JokesIconCircle>
+          <NewJokesBtn
+            className='btn btn-lg font-w-6'
+            onClick={() => fetchJokes(state.jokes, setState)}
+          >
+            New Jokes
+          </NewJokesBtn>
+        </CardHeader>
+        <div className='card-body p-0 flex-center'>
+          <StatsBody>
+            {Object.entries(state.jokesStats).map(([key, value]) => (
+              <li key={key} className='flex-b-30 flex-center'>
+                <i className={`flex-b-40 font-s-15 em ${key}`}></i>
+                <span className='flex-b-40 font-s-15 font-w-6'>{value}</span>
+              </li>
+            ))}
+          </StatsBody>
+        </div>
+        <div className='card-footer'>
+          <span className='font-s-15 font-w-6'>Total Jokes: {state.jokes.length}</span>
+        </div>
+      </JokesStats>
+      <JokesCard className='card flex-b-65'>
+        <CardBody className='card-body text-light scroll p-0'>
+          { (state.loading)
+            ? <Spinner />
+            : state.jokes.map(joke => <Joke key={joke.id} doVote={doVote} {...joke} />)
+          }
+        </CardBody>
+      </JokesCard>
+    </JokesWrapper>
   )
 }
 //#endregion
